816a2e03a4a2f1aad9c1ea72fdf0f24df7e2b741
NoName <88678726+LucasTylczak@users.noreply.github.com>
2024-02-06 19:51:40 +0100

    QR_Code_Generator.py

--- QR_Code_Generator.py
+++ QR_Code_Generator.py
@@ -1,10 +1,10 @@
 import qrcode
 
-print(╔═══════════════════════╦════════════════════════════════╦═══════════════════════╗)
-print(║  Dev  LucasTylczak   ║  Info   Qr code generator     ║  Programm   Python   ║)
-print(╚═══════════════════════╩════════════════════════════════╩═══════════════════════╝)
+print("╔═══════════════════════╦════════════════════════════════╦═══════════════════════╗")
+print("║  Dev : LucasTylczak   ║  Info : Qr code generator 	    ║  Programm : Python    ║")
+print("╚═══════════════════════╩════════════════════════════════╩═══════════════════════╝")
 
-def generate_qr_code(data, file_name)
+def generate_qr_code(data, file_name):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -14,14 +14,14 @@ def generate_qr_code(data, file_name)
     qr.add_data(data)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color=black, back_color=white)
+    img = qr.make_image(fill_color="black", back_color="white")
     img.save(file_name)
-    print(fLe code QR a été généré avec succès et enregistré sous le nom  {file_name})
+    print(f"Le code QR a été généré avec succès et enregistré sous le nom : {file_name}")
 
-if __name__ == __main__
-    print(Que voulez-vous encoder dans le code QR )
-    user_input = input(Entrez du texte, une URL ou des données structurées  )
+if __name__ == "__main__":
+    print("Que voulez-vous encoder dans le code QR ?")
+    user_input = input("Entrez du texte, une URL ou des données structurées : ")
 
-    file_name = input(Entrez le nom du fichier pour enregistrer le code QR (ex qr_code.png)  )
+    file_name = input("Entrez le nom du fichier pour enregistrer le code QR (ex: qr_code.png) : ")
 
     generate_qr_code(user_input, file_name)
